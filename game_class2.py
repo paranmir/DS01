1bc3ebf516b6d3ccfdeefe514e6adecbc90d1965
Baekchanwoo <bcw043785@gmail.com>
2019-11-30 15:33:08 +0900

    클래스화 한 메인로직. game_class1이 입력받기 위한 클래스 입니다. class_main은 추후 업데이트 예정

---
+++ game_class2.py
@@ -0,0 +1,126 @@
+import random
+import poker
+from poker import Suit
+from poker import Rank
+from poker import Card
+from poker.hand import Hand, Combo
+
+import game_jokbo
+import game_class
+
+class Print(game_class.GameInfo):     # 그냥 gui만들기 전에 값 전달 잘 되는지 확인해보려고 만들어본 클래스
+    def print1(self):
+        print(self.notice)
+        print("============================================")
+        print("player1: ",self.nameOfPlayer1)
+        print("         남은 돈: ",self.moneyOfPlayer1)
+        print("         패: ",self.handsOfPlayer1)
+        print("============================================")
+        print("player2: ",self.nameOfPlayer2)
+        print("         남은 돈: ",self.moneyOfPlayer2)
+        print("         패: ",self.handsOfPlayer2)
+        print("============================================")
+        print("전체: ")
+        print("         모인 돈: ",self.collectedBet)
+        print("         공통 카드: ",self.communityCards)
+        print("============================================")
+
+
+
+
+class GameProgress(Print):	# 선택을 입력받고 결과 출력하는 클래스.(게임은 한 사람이 선택을 할 때마다 진행된다.)
+    def __init(self):
+        self.choice=0     # 선택한 번호를 저장하는 변수.(이 부분은 수정해야 할 듯.)
+
+               
+    def betting_1(self): # big blind가 먼저 베팅을 하는 메소드.(일단 player1이 big blind라고 했음.)
+        
+        self.update_notice("big bind ("+self.nameOfPlayer1+") 님이 베팅을 선택하십시오: \n1: 베팅 \n2:체크 (베팅을 하지 않음.)")
+
+        self.print1()    # 출력
+
+        self.choice=input() # 선택을 입력받음.(이 부분은 수정해야 할 듯.)
+
+        if int(self.choice)==1:
+            self.update_notice("베팅 \nbig bind ("+self.nameOfPlayer1+") 님께서 베팅하실 금액을 정해주십시오: ")
+            self.print1()    # 출력
+            self.bet1=input()
+            self.append_moneyOfPlayer1(-int(self.bet1))
+            self.append_collectedBet(-int(self.bet1))
+
+        else:
+            self.update_notice("체크")
+    
+        self.print1()    # 출력
+
+    def afterBetting_1(self):			# 처음 한 사람이 베팅을 한 뒤, 그 다음 사람이 선택을 할 수 있는 함수 afterBetting의 선언
+                                                        # big bind가 먼저 선택을 한 뒤, small blind의 선택.
+
+        self.update_notice(self.nameOfPlayer2+"(small blind) 님께선 선택을 하십시오: \n1: 콜 (big blind와 같은 금액 베팅) \n2: 레이즈 (big blind보다 많은 금액 베팅) \n3: 폴드 (베팅을 포기)")
+        self.print1()    # 출력
+
+        self.choice=input()
+        if int(self.choice)==1:
+            self.update_notice("콜")
+            self.bet2=self.bet1
+            self.append_moneyOfPlayer2(-int(self.bet2))
+            self.append_collectedBet(self.bet2)
+
+        elif int(self.choice)==2:
+            self.update_notice("레이즈 \n 베팅할 금액을 입력하세요.(원래 베팅 금액보다 커야 합니다.)")            
+            self.bet2=input()
+            self.append_moneyOfPlayer2(-int(self.bet2))
+            self.append_collectedBet(self.bet2)
+
+        elif int(self.choice)==3:
+            self.update_notice("폴드")
+        
+        self.print1()    # 출력
+
+
+
+    def betting_2(self): # small blind가 먼저 베팅을 하는 메소드.(일단 player2가 small blind라고 했음.)
+        
+        self.update_notice("small bind ("+self.nameOfPlayer2+") 님이 베팅을 선택하십시오: \n1: 베팅 \n2:체크 (베팅을 하지 않음.)")
+
+        self.print1()    # 출력
+
+        self.choice=input() # 선택을 입력받음.(이 부분은 수정해야 할 듯.)
+
+        if int(self.choice)==1:
+            self.update_notice("베팅 \nsmall bind ("+self.nameOfPlayer2+") 님께서 베팅하실 금액을 정해주십시오: ")
+            self.bet1=input()
+            self.append_moneyOfPlayer2(-int(self.bet1))
+            self.append_collectedBet(self.bet1)
+
+        else:
+            self.update_notice("체크")
+    
+        self.print1()    # 출력
+
+    def afterBetting_2(self):			# 처음 한 사람이 베팅을 한 뒤, 그 다음 사람이 선택을 할 수 있는 함수 afterBetting의 선언
+                                                        # small bind가 먼저 선택을 한 뒤, big blind의 선택.
+
+        self.update_notice(self.nameOfPlayer1+"(big blind) 님께선 선택을 하십시오: \n1: 콜 (big blind와 같은 금액 베팅) \n2: 레이즈 (big blind보다 많은 금액 베팅) \n3: 폴드 (베팅을 포기)")
+        self.print1()    # 출력
+
+        self.choice=input()
+        if int(self.choice)==1:
+            self.update_notice("콜")
+            self.bet2=self.bet1
+            self.append_moneyOfPlayer1(-int(self.bet2))
+            self.append_collectedBet(self.bet2)
+
+        elif int(self.choice)==2:
+            self.update_notice("레이즈 \n 베팅할 금액을 입력하세요.(원래 베팅 금액보다 커야 합니다.)")            
+            self.bet2=input()
+            self.append_moneyOfPlayer1(-int(self.bet2))
+            self.append_collectedBet(self.bet2)
+
+        elif int(self.choice)==3:
+            self.update_notice("폴드")
+        
+        self.print1()    # 출력
+
+
+
---
+++ game_main.py
@@ -0,0 +1,20 @@
+import random
+import poker
+from poker import Suit
+from poker import Rank
+from poker import Card
+from poker.hand import Hand, Combo
+
+import game_jokbo
+import game_class
+import game_class2
+
+#preflop round
+
+#flop round
+
+#turn round
+
+#river round
+
+
